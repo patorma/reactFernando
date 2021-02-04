bf0bd83ad79480ea8184b23575ca873e30caa901
patorma <pocontreras@ing.ucsc.cl>
2021-02-03 21:56:57 -0300

    descripcion de un componente

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+import React  from 'react';
+import ReactDom from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const divRoot = document.querySelector('#root');
+
+// renderisar o mostrar vista
+// primer parametro el elemento 
+ReactDom.render(<PrimeraApp />,divRoot)
