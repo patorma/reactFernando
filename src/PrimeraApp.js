cc14b7467356b8a5dff63b90cdb4ef459707be71
patorma <pocontreras@ing.ucsc.cl>
2021-02-04 00:10:29 -0300

    fragment e impresion de variables en react

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,7 +3,16 @@ import React from 'react';
 
 const PrimeraApp = () =>{
 
-    return <h1>Hola Mundo</h1>
+    const saludo = 'Hola Mundo'
+
+    return ( // se regresa un objeto,3 es el espacio y null el remplace
+      <>  
+      <h1>{ saludo }</h1>
+      {/* <pre>{ JSON.stringify(saludo, null,3) }</pre> */}
+      <p>Mi primera aplicación</p>
+      </>
+    )
+   
 }
 
 export default PrimeraApp;
