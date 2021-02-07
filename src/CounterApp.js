ed1ec0b077748da40497bc821d1fdf57297867a7
patorma <pocontreras@ing.ucsc.cl>
2021-02-06 21:36:45 -0300

    se vio introducciona hooks y una primera forma de manejasr useStae

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
 
-    //handleAdd
-    const handleAdd = (e) =>{
-        console.log(e)
-    }
+  const [counter,setCounter] = useState(0);// retorna un arreglo
+
+
+
+
+  //handleAdd
+  const handleAdd = () => {
+      setCounter( counter + 1);
+  };
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
 
       <button onClick={handleAdd}>+1</button>
     </>
@@ -18,6 +23,6 @@ const CounterApp = ({ value }) => {
 };
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 export default CounterApp;
